01ac5a0f8848c122349deb7542f654f4fd88ff69
Alejandra <dil00ry@gmail.com>
2025-08-31 11:13:05 -0500

    Optimize code with a generic function to call HTML elements and modify the headers

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 let amigos = [];
 
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value.trim();
     console.log(nombreAmigo);
@@ -7,8 +13,10 @@ function agregarAmigo() {
     if (nombreAmigo === '') {
         alert('Por favor, inserte un nombre.');
     }
-    amigos.push(nombreAmigo);
-    console.log(amigos);
+        amigos.push(nombreAmigo);
+        console.log(amigos);
 
 }
 
+asignarTextoElemento('h1', '!Amigo Secreto!');
+asignarTextoElemento('h2', 'Escribe el nombre de tus amigos');
