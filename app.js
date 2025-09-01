cc6f64a3945fe414afe8cf48ccf869636f5afd52
Alejandra <dil00ry@gmail.com>
2025-09-01 16:35:55 -0500

    Clear the list so names don't duplicate

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ function agregarAmigo() {
 
 function enlistarAmigo() {
     let amigoAgregado = document.getElementById('listaAmigos')
+    amigoAgregado.innerHTML = "";
 }
 
 function limpiarCaja(){
