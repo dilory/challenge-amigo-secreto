e51995f0d06bc16ebef3b09eed2332fa46d04faa
Alejandra <dil00ry@gmail.com>
2025-09-06 06:20:18 -0500

    Update the length array verification and add an alert when the user tries to play without adding any names

--- app.js
+++ app.js
@@ -12,16 +12,12 @@ function agregarAmigo() {
     // obtiene el elemento del HTML, trim para evitar que quede el campo vacío
 
     if (nombreAmigo === '') {
-        alert('Por favor, agrega un nombre.');
-        // alerta para que el usuario evite agregar un elemento vacío a la lista
+        alert('Por favor, agrega un nombre.'); // alerta para que el usuario evite agregar un elemento vacío a la lista
     } else {
-        amigos.push(nombreAmigo);
-        // se agrega el nombre al array
+        amigos.push(nombreAmigo); // se agrega el nombre al array
         console.log(amigos);
-        limpiarCaja();
-        // función para limpiar el input después de agregar un nombre
-        enlistarAmigo();
-        // función para mostrar la lista de nombres que se van agregando
+        limpiarCaja(); // función para limpiar el input después de agregar un nombre
+        enlistarAmigo(); // función para mostrar la lista de nombres que se van agregando
     }
 
 }
@@ -34,18 +30,19 @@ function enlistarAmigo() {
     for (let i = 0; i < amigos.length; i++){
         // iniciazión; condición; actualización
         // loop para recorrer la lista, si 'i' no es menor al número total de nombres en 'amigos', el loop se detiene 
-        let lista = document.createElement('li');
-        // crea la lista
-        lista.textContent = amigos[i];
-        // capta el texto en el aaray y lo introduce en la lista
-        amigoAgregado.appendChild(lista);
-        // toma la nueva lista <li> y la agrega a <u> en el HTML
+        let lista = document.createElement('li'); // crea la lista
+        lista.textContent = amigos[i]; // capta el texto en el aaray y lo introduce en la lista
+        amigoAgregado.appendChild(lista); // toma la nueva lista <li> y la agrega a <u> en el HTML
     }
 }
 
 function sortearAmigo(){
-    console.log(amigos.length ? 'Hay amigos disponibles' : 'La lista está vacía');
+    // console.log(amigos.length ? 'Hay amigos disponibles' : 'La lista está vacía');
     // para verificar que el array no esté vacío al momento de sortear el amigo
+    if (amigos.length === 0) {
+        alert("No hay amigos disponibles para sortear");
+        return; // return para que se detenga la funcion aquí antes del random draw
+    }
 }
 
 function limpiarCaja(){
